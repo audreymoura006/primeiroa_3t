3f4fbd47ad7c6885f81913d88546655273ddbeac
audreymoura006 <108940130+audreymoura006@users.noreply.github.com>
2022-10-07 11:07:01 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -33,3 +33,11 @@ function quadrado(){
         document.write("O quadrado de " + i + " é " + (i*i)+ "<br>");
     }
 }
+
+
+function calcula(){
+    let c = document.getElementById("valor").value;
+    let j = document.getElementById("juros").value;
+    let r = c *(1 + (j/100));
+    document.write("Resultado: " + r);
+}
